1891f53922592000ac0e80b191ff046a5ac8176c
Li He <jet.in.brain@gmail.com>
2014-06-07 00:42:02 +0800

    integrate sencha touch skeleton app

---
+++ core/client/app/view/Main.js
@@ -0,0 +1,48 @@
+Ext.define('ICollege.view.Main', {
+    extend: 'Ext.tab.Panel',
+    xtype: 'main',
+    requires: [
+        'Ext.TitleBar',
+        'Ext.Video'
+    ],
+    config: {
+        tabBarPosition: 'bottom',
+
+        items: [
+            {
+                title: 'Woohoo!',
+                iconCls: 'home',
+
+                styleHtmlContent: true,
+                scrollable: true,
+
+                items: {
+                    docked: 'top',
+                    xtype: 'titlebar',
+                    title: 'Welcome to Sencha Touch 2'
+                },
+
+                html: [
+                    "I changed the default <b>HTML Contents</b> to something different!"
+                ].join("")
+            },
+            {
+                title: 'Get Started',
+                iconCls: 'action',
+
+                items: [
+                    {
+                        docked: 'top',
+                        xtype: 'titlebar',
+                        title: 'Getting Started'
+                    },
+                    {
+                        xtype: 'video',
+                        url: 'http://av.vimeo.com/64284/137/87347327.mp4?token=1330978144_f9b698fea38cd408d52a2393240c896c',
+                        posterUrl: 'http://b.vimeocdn.com/ts/261/062/261062119_640.jpg'
+                    }
+                ]
+            }
+        ]
+    }
+});
