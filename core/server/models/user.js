5cb5c6226c518d95a2d8fa2226c18bb01257ad7f
Li He <jet.in.brain@gmail.com>
2014-06-19 00:31:21 +0800

    refine models module base

--- core/server/models/user.js
+++ core/server/models/user.js
@@ -1,27 +1,18 @@
-var _              = require('lodash'),
-    when           = require('when'),
-    mongoose       = require('mongoose'),
+var _       = require('lodash'),
+    when    = require('when'),
+    mongoose = require('mongoose'),
     icollegeSchema = require('./base'),
-    errors         = require('../errors'),
-    schema     = require('../data/schema'),
+    errors  = require('../errors'),
+    schema  = require('../data/schema'),
 
     User,
     Users,
     collection = "users";
 
 
-// Schema definition
-Users = new mongoose.Schema(schema.collections[collection], _.merge({
-    collection: collection
-}, icollegeSchema.options));
-
-// Model definition
-User = mongoose.model('User', Users);
-
-
 // Schema instance methods
 var methods = {
-    'findSameNames': function(cb) {
+    'findSameNames': function (cb) {
         // this refers to Model Object
         return this.model('User').find({ name: this.name }, cb);
     }
@@ -29,15 +20,35 @@ var methods = {
 
 // Schema static methods
 var statics = {
-    'findByName': function(name, cb) {
+    'findByName': function (name, cb) {
         // this refers to Schema Object
         this.find({ name: new RegExp(name, 'i') }, cb);
     }
 };
 
-_.merge(statics, icollegeSchema.statics);
-_.merge(Users.methods, methods);
-_.merge(Users.statics, statics);
+
+// Schema definition
+Users = new mongoose.Schema(
+    // field and type and validations
+    schema.collections[collection],
+    // schema options
+    _.extend({}, icollegeSchema.options,
+        {
+            collection: collection
+        }
+    )
+);
+
+// Model definition
+User = mongoose.model('User', Users);
+
+
+_.extend(Users.statics, icollegeSchema.statics, statics);
+_.extend(Users.methods, methods);
+
+_.mapValues(icollegeSchema.plugins, function(plugin){
+    Users.plugin(plugin);
+});
 
 
 module.exports = {
--- core/test/test.js
+++ core/test/test.js
@@ -3,19 +3,3 @@ var base = require('../server/storage/base'),
     _   = require('lodash');
 
 console.log(base.getTargetDir('hello'));
-
-var a = {
-    name: 'lihe',
-    age : 9
-};
-
-var b = {
-    name: 'haha',
-    sex: 'm'
-};
-
-
-console.log(_.merge(a, b));
-
-console.log(a);
-
