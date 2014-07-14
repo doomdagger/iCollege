3f279402d3a741503dc1ea36a108e0a26e19570a
Li He <jet.in.brain@gmail.com>
2014-07-14 18:58:18 +0800

    update schema.js

--- core/server/data/schema.js
+++ core/server/data/schema.js
@@ -98,12 +98,66 @@ var db = {
 
     // ### 消息实体 一定要考虑消息的共通性，系统消息和聊天消息
     messages: {
-
+        uuid: {type: String}, // uuid
+        content: {type: String},
+        source_category: {type: String}, // 消息来自于系统，还是个人（好友之间），还是群组
+        content_category: {type: String}, // 消息类型：多媒体消息（视频，纯图片，音频），富文本消息(html)，其他类型的系统消息（好友请求，其他由系统relay的具有特殊格式的消息），
+        message_from: {type: ObjectId}, // 从这也能看出来，账户必须有角色，角色具有权限分级，并预留一个账户具备超级管理员角色，可以赋予普通用户管理员角色
+        message_to: {type: ObjectId}, // to的多样性，用户，群组
+        being_pulled: {type: Boolean}, // 前端是否曾经抓取过
+        created_at: {type: Date},
+        created_by: {type: ObjectId},
+        updated_at: {type: Date},
+        updated_by: {type: ObjectId} // 消息的某些状态被改变，改变者为谁
     },
 
-    // ### 帖子实体 一定要考虑帖子的共通性，发帖（日志），回帖；存在于圈子里（所以人，都有一个自己的圈子，好友圈，其他圈子都是额外创建的）
+    // ### 帖子实体 一定要考虑帖子的共通性，发帖（日志）；存在于圈子里（所以人，都有一个自己的圈子，好友圈，其他圈子都是额外创建的）
     posts: {
+        uuid: {type: String},
+        title: {type: String},
+        slug: {type: String}, // 消息的唯一标识符，全局分配，会被用于url中
+        html: {type: String}, // 帖子内容
+        attachments: [{
+            name: {type: String},
+            extension: {type: String}, // 文件扩展类型
+            size: {type: Number},
+            path: {type: String}, // 文件路径
+            created_at: {type: Date},
+            created_by: {type: ObjectId}
+        }],
+        featured: {type: Boolean}, // 特色贴
+        status: {type: String}, // 帖子的状态，draft or published
+        language: {type: String},
+        source_category: {type: String}, // 帖子的来源，好友圈（默认），或是其他某一个圈子：好友圈这个圈子其实并不存在，这个圈子包含了用户的所有好友
+        post_from: {type: ObjectId}, // 用户Id，谁发的帖子
+        created_at: {type: Date},
+        created_by: {type: ObjectId},
+        updated_at: {type: Date},
+        updated_by: {type: ObjectId},
+        published_at: {type: Date},
+        published_by: {type: ObjectId}
+    },
 
+    // ### 回帖实体
+    re_posts: {
+        uuid: {type: String},
+        html: {type: String}, // 帖子内容
+        attachments: [{
+            name: {type: String},
+            extension: {type: String}, // 文件扩展类型
+            size: {type: Number},
+            path: {type: String}, // 文件路径
+            created_at: {type: Date},
+            created_by: {type: ObjectId}
+        }],
+        language: {type: String},
+        source_category: {type: String}, // 是来自于帖子 还是 回帖
+        re_post_from: {type: ObjectId}, // 用户Id，谁发的帖子回复
+        re_post_to: {type: ObjectId}, // 帖子ID或是回帖ID
+        created_at: {type: Date},
+        created_by: {type: ObjectId},
+        updated_at: {type: Date},
+        updated_by: {type: ObjectId}
     }
 
     /*
