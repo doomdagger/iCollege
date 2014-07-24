83f5e1dbdeac163e14ca0ecc1b406db54d527c1c
Li He <jet.in.brain@gmail.com>
2014-07-24 16:34:52 +0800

    add fixture for icollege, some data needed to be populated for test or something else

--- core/server/data/fixtures/index.js
+++ core/server/data/fixtures/index.js
@@ -3,10 +3,33 @@
  * Created by Li He on 2014/7/24.
  * Use this class to populate or update database data
  */
+var sequence    = require('when/sequence'),
+    _           = require('lodash'),
+    node_uuid   = require('node-uuid'),
+    Role        = require('../../models/role').Role,
+    Permission  = require('../../models/permission').Permission,
+    Permissions = require('../../models/permission').Permissions,
 
+    populateFixtures,
+    updateFixtures;
 
 // before handle, populate demo user or posts or message data
 // into database.
 var fixtures = {
+
+    permissions: [
+        {
+            uuid: node_uuid.v4(),
+            name: ""
+        }
+    ],
+
+    roles: [
+        {
+            uuid: node_uuid.v4(),
+            name: 'SuperAdministrator',
+
+        }
+    ]
 
 };
