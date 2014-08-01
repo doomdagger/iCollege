c5ce9d88c9f04fb1ea20ff5475c34f2037e42a02
Li He <jet.in.brain@gmail.com>
2014-08-01 19:22:19 +0800

    update fixture.js

--- core/server/data/fixtures/index.js
+++ core/server/data/fixtures/index.js
@@ -64,7 +64,7 @@ var fixtures = {
     ],
 
     permissions: [
-        // admin
+        // admin - all
         {
             uuid: node_uuid.v4(),
             name: "Browse users",
@@ -78,7 +78,7 @@ var fixtures = {
             object_type: "app",
             action_type: "browse"
         },
-        // user - me
+        // user - me (field: user_id)
         {
             uuid: node_uuid.v4(),
             name: "Browse notification",
@@ -99,35 +99,44 @@ var fixtures = {
             object_type: "circle",
             action_type: "browse"
         },
-        // user - related (group messages are related to me, friends' messages sent to me are also related to me)
-        {
-            uuid: node_uuid.v4(),
-            name: "Browse messages",
-            object_type: "message",
-            action_type: "browse"
-        },
-        // post - related (friends circle's posts, other circles' posts, my own posts)
+    /**
+     * 这个业务的权限以及权限的scope太难以描述，且针对性比较强，
+     * 此业务方法的功用是为了拉取未读消息，或根据时间戳同步本地消息
+     * 所以绝对单独定义业务方法，不在权限里限定
+     */
+//        // user - related (group messages are related to me, friends' messages sent to me are also related to me)
+//        {
+//            uuid: node_uuid.v4(),
+//            name: "Browse messages",
+//            object_type: "message",
+//            action_type: "browse"
+//        },
+
+        // post - related (field: circle_id; related circles' posts)
+        // post - me (field: user_id; my own posts)
         {
             uuid: node_uuid.v4(),
             name: "Browse posts",
             object_type: "post",
             action_type: "browse"
         },
-        // repost - related
+
+        // repost - related (field: circle_id; related circles' reposts)
+        // repost - me (field: user_id; my own reposts)
         {
             uuid: node_uuid.v4(),
             name: "Browse reposts",
             object_type: "repost",
             action_type: "browse"
         },
-        // admin
+        // admin - all
         {
             uuid: node_uuid.v4(),
             name: "Browse settings",
             object_type: "setting",
             action_type: "browse"
         },
-        // user - all (affected by user's profile_visibility)
+        // user - all (affected by user's profile_visibility, if it is me, show it)
         {
             uuid: node_uuid.v4(),
             name: "Read user",
@@ -141,252 +150,262 @@ var fixtures = {
             object_type: "app",
             action_type: "read"
         },
-        // user - me
+        // user - me (field: user_id)
         {
             uuid: node_uuid.v4(),
             name: "Read notification",
             object_type: "notification",
             action_type: "read"
         },
-        // user - all
+        // user - all (only show partial info to non-member user, show all to group members)
         {
             uuid: node_uuid.v4(),
             name: "Read group",
             object_type: "group",
             action_type: "read"
         },
-        // user - all
+        // user - all (only show partial info to non-member user, show all to circle members)
         {
             uuid: node_uuid.v4(),
             name: "Read circle",
             object_type: "circle",
             action_type: "read"
         },
-        // user - related
+        // user - related (field: circle_id)
+        // user - me (field: user_id)
         {
             uuid: node_uuid.v4(),
             name: "Read post",
             object_type: "post",
             action_type: "read"
         },
-        // user - related
+        // user - related (field: circle_id)
+        // user - me (field: user_id)
         {
             uuid: node_uuid.v4(),
             name: "Read repost",
             object_type: "repost",
             action_type: "read"
         },
-        // admin
+        // admin - all
         {
             uuid: node_uuid.v4(),
             name: "Read setting",
             object_type: "setting",
             action_type: "read"
         },
-        // user - me
+        // user - me (field: _id)
         {
             uuid: node_uuid.v4(),
             name: "Edit user",
             object_type: "user",
             action_type: "edit"
         },
-        // admin
+        // admin - all
         {
             uuid: node_uuid.v4(),
             name: "Edit app",
             object_type: "app",
             action_type: "edit"
         },
-        // admin
+        // admin - all
         {
             uuid: node_uuid.v4(),
             name: "Edit notification",
             object_type: "notification",
             action_type: "edit"
         },
-        // user - related
+        // user - related (field: members.member_id & members.related; 如果是群管理员，可以edit group)
+        // user - me (field: user_id)
         {
             uuid: node_uuid.v4(),
             name: "Edit group",
             object_type: "group",
             action_type: "edit"
         },
-        // user - related
+        // user - related (field: members.member_id & members.related; 如果是圈子管理员，可以edit circle)
+        // user - me (field: user_id)
         {
             uuid: node_uuid.v4(),
             name: "Edit circle",
             object_type: "circle",
             action_type: "edit"
         },
-        // admin or internal(mostly: being_pulled)
+        // admin - all
+        // internal (mostly: being_pulled)
         {
             uuid: node_uuid.v4(),
             name: "Edit message",
             object_type: "message",
             action_type: "edit"
         },
-        // user - me or internal(sometimes: favor or view)
+        // user - me （field: user_id)
+        // internal (sometimes: favored_users or viewed_times)
         {
             uuid: node_uuid.v4(),
             name: "Edit post",
             object_type: "post",
             action_type: "edit"
         },
-        // admin
+        // admin - all
         {
             uuid: node_uuid.v4(),
             name: "Edit repost",
             object_type: "repost",
             action_type: "edit"
         },
-        // admin
+        // admin - all
         {
             uuid: node_uuid.v4(),
             name: "Edit setting",
             object_type: "setting",
             action_type: "edit"
         },
-        // admin or internal(sign up)
+        // admin - all
+        // internal(sign up)
         {
             uuid: node_uuid.v4(),
             name: "Add user",
             object_type: "user",
             action_type: "add"
         },
-        // admin
+        // admin - all
         {
             uuid: node_uuid.v4(),
             name: "Add app",
             object_type: "app",
             action_type: "add"
         },
-        // admin
+        // admin - all
         {
             uuid: node_uuid.v4(),
             name: "Remove user",
             object_type: "user",
             action_type: "remove"
         },
-        // admin
+        // admin - all
         {
             uuid: node_uuid.v4(),
             name: "Remove app",
             object_type: "app",
             action_type: "remove"
         },
-        // admin
+        // admin - all
         {
             uuid: node_uuid.v4(),
             name: "Remove notification",
             object_type: "notification",
             action_type: "remove"
         },
-        // user - me
+        // user - me (field: user_id)
         {
             uuid: node_uuid.v4(),
             name: "Remove group",
             object_type: "group",
             action_type: "remove"
         },
-        // user - me
+        // user - me (field: user_id)
         {
             uuid: node_uuid.v4(),
             name: "Remove circle",
             object_type: "circle",
             action_type: "remove"
         },
-        // admin
+        // admin - all
         {
             uuid: node_uuid.v4(),
             name: "Remove message",
             object_type: "message",
             action_type: "remove"
         },
-        // user - me
+        // user - me (field: user_id)
         {
             uuid: node_uuid.v4(),
             name: "Remove post",
             object_type: "post",
             action_type: "remove"
         },
-        // user - related (post owner can remove post's reposts)
+        // user - me (field: user_id)
         {
             uuid: node_uuid.v4(),
             name: "Remove repost",
             object_type: "repost",
             action_type: "remove"
         },
-        // admin
+        // admin - all
         {
             uuid: node_uuid.v4(),
             name: "Create notification",
             object_type: "notification",
             action_type: "create"
         },
-        // user - me
+        // user - me (no field; other models can reject the create if the user cannot create due to a limited permissions)
         {
             uuid: node_uuid.v4(),
             name: "Create group",
             object_type: "group",
             action_type: "create"
         },
-        // user - me
+        // user - me (no field; other models can reject the create if the user cannot create due to a limited permissions)
         {
             uuid: node_uuid.v4(),
             name: "Create circle",
             object_type: "circle",
             action_type: "create"
         },
-        // user - me
+        // user - me (no field; other models can reject the create if the user cannot create due to a limited permissions)
         {
             uuid: node_uuid.v4(),
             name: "Create message",
             object_type: "message",
             action_type: "create"
         },
-        // user - me
+        // user - me (no field; other models can reject the create if the user cannot create due to a limited permissions)
         {
             uuid: node_uuid.v4(),
             name: "Create post",
             object_type: "post",
             action_type: "create"
         },
-        // user - related (If I can browse, then I can favor)
-        {
-            uuid: node_uuid.v4(),
-            name: "Favor post",
-            object_type: "post",
-            action_type: "favor"
-        },
-        // user - related (If I can browse, then I can favor)
-        {
-            uuid: node_uuid.v4(),
-            name: "Favor repost",
-            object_type: "repost",
-            action_type: "favor"
-        },
-        // user - related
+        // user - me (no field; other models can reject the create if the user cannot create due to a limited permissions)
         {
             uuid: node_uuid.v4(),
             name: "Create repost",
             object_type: "repost",
             action_type: "create"
         },
-        // admin
+        // user - related (field: circle_id; If I can browse, then I can favor)
+        // user - me (field: user_id)
+        {
+            uuid: node_uuid.v4(),
+            name: "Favor post",
+            object_type: "post",
+            action_type: "favor"
+        },
+        // user - related (field: circle_id; If I can browse, then I can favor)
+        // user - me (field: user_id)
+        {
+            uuid: node_uuid.v4(),
+            name: "Favor repost",
+            object_type: "repost",
+            action_type: "favor"
+        },
+
+        // admin - all
         {
             uuid: node_uuid.v4(),
             name: "Export database",
             object_type: "db",
             action_type: "exportContent"
         },
-        // admin
+        // admin - all
         {
             uuid: node_uuid.v4(),
             name: "Import database",
             object_type: "db",
             action_type: "importContent"
         },
-        // admin
+        // admin - all
         {
             uuid: node_uuid.v4(),
             name: "Delete all content",
@@ -394,7 +413,9 @@ var fixtures = {
             action_type: "deleteAllContent"
         },
 
-        // user - related (add member for my own group or add member for group in which I am a co-admin )
+        // user - related (field: members.member_id & members.related)
+        // user - me (field: user_id)
+        // (add member for my own group or add member for group in which I am a co-admin )
         // do not misunderstood this with some api interface, this permission describe whether a user or app
         // have the permission to add someone into a specific group
         {
@@ -403,7 +424,9 @@ var fixtures = {
             object_type: "member",
             action_type: "add"
         },
-        // user - related (add member for my own circle or add member for circle in which I am a co-admin )
+        // user - related (field: members.member_id & members.related)
+        // user - me (field: user_id)
+        // (add member for my own circle or add member for circle in which I am a co-admin )
         // do not misunderstood this with some api interface, this permission describe whether a user or app
         // have the permission to add someone into a specific circle
         {
@@ -412,42 +435,54 @@ var fixtures = {
             object_type: "member",
             action_type: "add"
         },
-        // user - related (me or others(if I am owner of group or a co-admin))
+        // user - related (field: members.member_id & members.related)
+        // user - me (field: user_id)
+        // editing member can only edit 'member_name' field
         {
             uuid: node_uuid.v4(),
             name: "Edit group member",
             object_type: "member",
             action_type: "edit"
         },
-        // user - related (me or others(if I am owner of circle or a co-admin))
+        // user - related (field: members.member_id & members.related)
+        // user - me (field: user_id)
+        // editing member can only edit 'member_name' field
         {
             uuid: node_uuid.v4(),
             name: "Edit circle member",
             object_type: "member",
             action_type: "edit"
         },
-        // user - related (me or others(if I am owner of group or a co-admin))
+        // user - related (field: members.member_id & members.related)
+        // user - me (field: user_id)
+        // (me or others(if I am owner of group or a co-admin))
         {
             uuid: node_uuid.v4(),
             name: "Remove group member",
             object_type: "member",
             action_type: "remove"
         },
-        // user - related (me or others(if I am owner of circle or a co-admin))
+        // user - related (field: members.member_id & members.related)
+        // user - me (field: user_id)
+        // (me or others(if I am owner of group or a co-admin))
         {
             uuid: node_uuid.v4(),
             name: "Remove circle member",
             object_type: "member",
             action_type: "remove"
         },
-        // user - related (if I own the group or I am a member of it)
+        // user - related (field: members.member_id)
+        // user - me (field: user_id)
+        // (me or others(if I am owner of group or a co-admin))
         {
             uuid: node_uuid.v4(),
             name: "Browse group members",
             object_type: "member",
             action_type: "browse"
         },
-        // user - related (if I own the circle or I am a member of it)
+        // user - related (field: members.member_id)
+        // user - me (field: user_id)
+        // (me or others(if I am owner of group or a co-admin))
         {
             uuid: node_uuid.v4(),
             name: "Browse circle members",
