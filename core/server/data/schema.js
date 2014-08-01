66b358aaa43faaec6920a19fb010aa4474aba7a8
Li He <jet.in.brain@gmail.com>
2014-08-01 21:01:44 +0800

    update schema.js

--- core/server/data/schema.js
+++ core/server/data/schema.js
@@ -85,8 +85,8 @@ var db = {
         }],
         permissions: [{
             permission_id: {type: Schema.Types.ObjectId, required: true},
-            permission_scope: {type: String, enum: ['all', 'related', 'me']} // 最高Scope，无限制，有关联，仅限自己
-            // object_id: {type: Schema.Types.ObjectId} // if the scope is 'related', an object id is not allowed to be optionally provided
+            permission_scope: {type: String, enum: ['all', 'related', 'me']}, // 最高Scope，无限制，有关联，仅限自己
+            object_id: {type: Schema.Types.ObjectId} // if the scope is 'related', an object id is not allowed to be optionally provided
         }],
         apps: [{
             app_id: {type: Schema.Types.ObjectId, required: true},
@@ -131,7 +131,7 @@ var db = {
         // app 能够拥有的permission永远是user permission的子集，这点需要验证！！！
         permissions: [{
             permission_id: {type: Schema.Types.ObjectId, required: true},
-            permission_scope: {type: String, enum: ['all', 'related', 'me']} // 最高Scope，无限制，有关联，仅限自己
+            permission_scope: {type: String, enum: ['all', 'me']} // 最高Scope，无限制，有关联，仅限自己
         }]
     },
 
@@ -155,7 +155,7 @@ var db = {
         name: {type: String, enum: ['SuperAdministrator', 'Administrator', 'iColleger'], required: true},
         permissions: [{
             permission_id: {type: Schema.Types.ObjectId, required: true},
-            permission_scope: {type: String, enum: ['all', 'related', 'me']} // 最高Scope，无限制，有关联，仅限自己
+            permission_scope: {type: String, enum: ['all', 'me']} // 最高Scope，无限制，有关联，仅限自己
         }],
         description: {type: String, trim: true, default: ""},
         created_at: {type: Date, default: Date.now()},
